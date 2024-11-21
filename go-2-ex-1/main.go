61f6a39c2377ccc5df3a97e23e966a1cdb633d57
PHIL HÄGELI <PHIL_HAEGELI@SLUZ.CH>
2024-11-21 16:22:15 +0100

    Aufgabe 4

--- go-2-ex-1/main.go
+++ go-2-ex-1/main.go
@@ -4,12 +4,21 @@ import "fmt"
 
 type FullName struct {
 	// TODO: add fields
+	FirstName string
+	LastName  string
 }
 
 // TODO: declare a structure for birth date
+type BirthDate struct {
+	Day   byte
+	Month byte
+	Year  uint16
+}
 
 type Profile struct {
 	// TODO: embed full name and birth date information
+	FullName
+	BirthDate
 	NumberOfSiblings byte
 	ZodiacSign       rune
 }
@@ -17,12 +26,22 @@ type Profile struct {
 func main() {
 	var me = Profile{
 		// TODO: set name and birth date information
-		NumberOfSiblings: 0,   // TODO: adjust
-		ZodiacSign:       ' ', // TODO: adjust
+		FullName: FullName{
+			FirstName: "Phil",
+			LastName:  "Hägeli",
+		},
+		BirthDate: BirthDate{
+			Day:   10,
+			Month: 3,
+			Year:  2008,
+		},
+		NumberOfSiblings: 2,   // TODO: adjust
+		ZodiacSign:       '♓', // TODO: adjust
 	}
 	fmt.Println(me)
 
 	fmt.Println("Siblings Before:", me.NumberOfSiblings)
 	// TODO: imagine, you get a little brother or sister
+	me.NumberOfSiblings++
 	fmt.Println("Siblings After:", me.NumberOfSiblings)
 }
