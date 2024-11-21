6ca3d88ffcb7644a43ab31f5fe51221502e01c90
PHIL HÄGELI <PHIL_HAEGELI@SLUZ.CH>
2024-11-21 15:38:23 +0100

    Fix Aufgabe 1

--- go-1-ex-1/main.go
+++ go-1-ex-1/main.go
@@ -11,7 +11,7 @@ func main() {
 	yearOfBirth := 2008
 	numberOfSiblings := 2
 	heightInMeters := 1.81
-	zodiacSign := 'F'
+	zodiacSign := '\u2653'
 
 	fmt.Printf("Vor- und Nachname: %s %s\n", firstName, lastName)
 	fmt.Printf("Geburtsdatum: %d.%d.%d\n", dayOfBirth, monthOfBirth, yearOfBirth)
