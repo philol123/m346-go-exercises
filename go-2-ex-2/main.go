a229541105a778366492dbfdb3e4cd826909d2df
PHIL HÄGELI <PHIL_HAEGELI@SLUZ.CH>
2024-11-28 15:15:19 +0100

    Aufgabe Fibonacci-Zahlen fertig

--- go-2-ex-2/main.go
+++ go-2-ex-2/main.go
@@ -6,9 +6,14 @@ func main() {
 	var fibs = []int{1, 1, 0, 0, 0}
 
 	fibs[2] = fibs[0] + fibs[1]
+	fibs[3] = fibs[1] + fibs[2]
+	fibs[4] = fibs[2] + fibs[3]
 	// TODO: correct up to index 4 using direct element access
 
-	fibs = append(fibs, 0) // TODO: replace 0 with the next Fibonacci number
+	fibs = append(fibs, fibs[3]+fibs[4])
+	fibs = append(fibs, fibs[4]+fibs[5])
+	fibs = append(fibs, fibs[5]+fibs[6])
+	fibs = append(fibs, fibs[6]+fibs[7])
 	// TODO: compute three more Fibonacci numbers and append them
 
 	fmt.Println(fibs) // expected output: [1 1 2 3 5 8 13 21 34]
