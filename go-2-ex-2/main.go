7edb269df87b97c171e33398d3c131c304c84434
PHIL HÄGELI <PHIL_HAEGELI@SLUZ.CH>
2024-11-28 15:30:31 +0100

    Zusätliche Fibonacci-Zahlen & Modul Aufgabe

--- go-2-ex-2/main.go
+++ go-2-ex-2/main.go
@@ -14,6 +14,11 @@ func main() {
 	fibs = append(fibs, fibs[4]+fibs[5])
 	fibs = append(fibs, fibs[5]+fibs[6])
 	fibs = append(fibs, fibs[6]+fibs[7])
+	fibs = append(fibs, fibs[7]+fibs[8])
+	fibs = append(fibs, fibs[8]+fibs[9])
+	fibs = append(fibs, fibs[9]+fibs[10])
+	fibs = append(fibs, fibs[10]+fibs[11])
+
 	// TODO: compute three more Fibonacci numbers and append them
 
 	fmt.Println(fibs) // expected output: [1 1 2 3 5 8 13 21 34]
--- go-2-ex-3/main.go
+++ go-2-ex-3/main.go
@@ -4,13 +4,20 @@ import "fmt"
 
 func main() {
 	// TODO: create a map called "modules"
-
+	modules := map[int]string{
+		104: "Datenmodell implementieren",
+		117: "Informatik- und Netzinfrastruktur für ein kleines Unternehmen realisieren",
+		346: "Cloud-Lösungen konzipieren und realisieren",
+	}
 	fmt.Println("Modul 104:", modules[104])
 	fmt.Println("Modul 117:", modules[117])
 	fmt.Println("Modul 346:", modules[346])
 
 	// TODO: delete one
+	delete(modules, 117)
 	// TODO: add one
+	modules[205] = "Netzwerkgrundlagen verstehen"
 	// TODO: replace one
+	modules[346] = "Office Werkzeuge anwenden"
 	fmt.Println(modules)
 }
