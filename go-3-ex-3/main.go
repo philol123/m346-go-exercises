44ff96a42b0723c20bd9aa5bf751dbd544634d6f
PHIL HÄGELI <PHIL_HAEGELI@SLUZ.CH>
2024-12-05 15:23:09 +0100

    3.3

--- go-3-ex-3/main.go
+++ go-3-ex-3/main.go
@@ -1,10 +1,23 @@
 package main
 
+import "fmt"
+
 const (
 	Lower = 1
 	Upper = 30
 )
 
 func main() {
-	// TODO: Implement FizzBuzz using a for loop from Lower to Upper.
+	for i := Lower; i <= Upper; i++ {
+		switch {
+		case i%3 == 0 && i%5 == 0:
+			fmt.Println("FizzBuzz")
+		case i%3 == 0:
+			fmt.Println("Fizz")
+		case i%5 == 0:
+			fmt.Println("Buzz")
+		default:
+			fmt.Println(i)
+		}
+	}
 }
