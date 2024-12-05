326f44d8b77df167fbae75743e7a216c066faafd
PHIL HÄGELI <PHIL_HAEGELI@SLUZ.CH>
2024-12-05 16:02:00 +0100

    3.4 mit zusatz

--- go-3-ex-4/main.go
+++ go-3-ex-4/main.go
@@ -3,28 +3,47 @@ package main
 import "fmt"
 
 const (
-	Diamonds = '\u25c6' // Karo
-	Spades   = '\u2660' // Pik
-	Clubs    = '\u2663' // Kreuz
-	Hearts   = '\u2665' // Herz
+	Diamonds = "♦" // Karo
+	Spades   = "♠" // Pik
+	Clubs    = "♣" // Kreuz
+	Hearts   = "♥" // Herz
 
-	Six   = '\u2465'
-	Seven = '\u2466'
-	Eight = '\u2467'
-	Nine  = '\u2468'
-	Ten   = '\u2469'
-	Jack  = 'J'
-	Queen = 'Q'
-	King  = 'K'
-	Ace   = 'A'
+	Six   = "6"
+	Seven = "7"
+	Eight = "8"
+	Nine  = "9"
+	Ten   = "10"
+	Jack  = "J"
+	Queen = "Q"
+	King  = "K"
+	Ace   = "A"
 )
 
 func main() {
-	suits := []rune{Diamonds, Spades, Clubs, Hearts}
-	ranks := []rune{Six, Seven, Eight, Nine, Ten, Jack, Queen, King, Ace}
+	suits := []string{Diamonds, Spades, Clubs, Hearts}
+	ranks := []string{Six, Seven, Eight, Nine, Ten, Jack, Queen, King, Ace}
 
-	// TODO: Loop over suits and ranks to output all combinations.
+	fmt.Println("Karten für jedes Symbol (Farbe) und Wert:")
+	for _, rank := range ranks {
+		for _, suit := range suits {
+			fmt.Printf("%s%s\t", suit, rank)
+		}
+		fmt.Println()
+	}
 
-	// TODO: delete this line afterwards
-	fmt.Println(suits, ranks)
+	cards := map[string][]string{
+		Diamonds: {"6", "7", "8", "9", "10", "J", "Q", "K", "A"},
+		Spades:   {"6", "7", "8", "9", "10", "J", "Q", "K", "A"},
+		Clubs:    {"6", "7", "8", "9", "10", "J", "Q", "K", "A"},
+		Hearts:   {"6", "7", "8", "9", "10", "J", "Q", "K", "A"},
+	}
+
+	fmt.Println("\nAusgabe der Karten nach Farben:")
+	for _, suit := range suits {
+		fmt.Printf("\nFarbe: %s\n", suit)
+		for _, card := range cards[suit] {
+			fmt.Printf("%s%s\t", suit, card)
+		}
+		fmt.Println()
+	}
 }
