11845909d66ab8b0b8fc3285afcb6a9a25d484c2
PHIL HÄGELI <PHIL_HAEGELI@SLUZ.CH>
2024-12-12 15:12:36 +0100

    4.2

--- go-4-ex-2/main.go
+++ go-4-ex-2/main.go
@@ -1,7 +1,27 @@
 package main
 
-// TODO: implement the function computeHypotenuse using math.Pow and math.Sqrt
+import (
+	"fmt"
+	"math"
+)
+
+type ShortSides struct {
+	a float64
+	b float64
+}
+
+func computeHypotenuse(a, b float64) float64 {
+	return math.Sqrt(math.Pow(a, 2) + math.Pow(b, 2))
+}
+
+func (s ShortSides) Hypotenuse() float64 {
+	return math.Sqrt(math.Pow(s.a, 2) + math.Pow(s.b, 2))
+}
 
 func main() {
-	// TODO: call the function computeHypotenuse
+	a, b := 3.0, 4.0
+	fmt.Printf("Länge der Hypotenuse (computeHypotenuse): %.2f\n", computeHypotenuse(a, b)) // Output: 5.00
+
+	sides := ShortSides{a: 5.0, b: 12.0}
+	fmt.Printf("Länge der Hypotenuse (Hypotenuse Methode): %.2f\n", sides.Hypotenuse()) // Output: 13.00
 }
