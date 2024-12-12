efe15588c9889457093c9307ac31ddf4b0cf0a36
PHIL HÄGELI <PHIL_HAEGELI@SLUZ.CH>
2024-12-12 15:03:47 +0100

    4.1

--- go-4-ex-1/main.go
+++ go-4-ex-1/main.go
@@ -1,7 +1,39 @@
 package main
 
-// TODO: implement the function computeGrade
+import (
+	"errors"
+	"fmt"
+)
+
+func computeGrade(gotPoints, maxPoints float64) (float64, error) {
+	if gotPoints < 0 || maxPoints <= 0 || gotPoints > maxPoints {
+		return 0, errors.New("ungültige Punktzahlen: überprüfe die Eingabewerte")
+	}
+
+	grade := (gotPoints / maxPoints * 5) + 1
+
+	return grade, nil
+}
 
 func main() {
-	// TODO: call the function computeGrade
+	grade, err := computeGrade(17.5, 28.0)
+	if err != nil {
+		fmt.Println("Fehler:", err)
+	} else {
+		fmt.Println("Note für 17.5 von 28.0:", grade)
+	}
+
+	grade, err = computeGrade(50, 50)
+	if err != nil {
+		fmt.Println("Fehler:", err)
+	} else {
+		fmt.Println("Note für 50 von 50:", grade)
+	}
+
+	grade, err = computeGrade(30, 20)
+	if err != nil {
+		fmt.Println("Fehler:", err)
+	} else {
+		fmt.Println("Note für 30 von 20:", grade)
+	}
 }
